44cf242a46a6b6601b6ee6181dd7eb1401244866
Alexander Ruberto <rubertoalexander@gmail.com>
2020-07-01 22:17:59 +1000

    Moved indicators into Stock object

--- Stock.py
+++ Stock.py
@@ -1,9 +1,11 @@
 import yfinance as yf
+from talib._ta_lib import *
 
 class Stock:
 
-    def __init__(self, ticker):
-        self.ticker = yf.Ticker(ticker)
+    def __init__(self, code):
+        self.code = code
+        self.ticker = yf.Ticker(code)
 
     def getData(self, period, interval):
         history = self.ticker.history(period, interval)
@@ -13,3 +15,13 @@ class Stock:
         self.closeList = history['Close']
         self.highList = history['High']
         self.lowList = history['Low']
+
+    def defineIndicators(self):
+        highList = self.highList
+        lowList = self.lowList
+        closeList = self.closeList
+        
+        self.posDI = PLUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
+        self.negDI = MINUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
+        self.rsi = RSI(closeList, timeperiod=14)
+        self.stochk, self.stochd = STOCH(high=highList, low=lowList, close=closeList, fastk_period=5, slowk_period=3, slowd_period=3)
