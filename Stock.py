b0c5b38bf09b65f2b642ccc0e0f56bb2d0ee702f
Alexander Ruberto <rubertoalexander@gmail.com>
2020-07-26 15:55:50 +1000

    Setting up multiple time frames

--- Stock.py
+++ Stock.py
@@ -8,22 +8,40 @@ class Stock:
         self.ticker = yf.Ticker(code)
         self.units = 0
         self.boughtAt = 0.00
+        self.greenLight = False
 
     def getData(self, period, interval):
         history = self.ticker.history(period, interval)
         history = history.drop(columns=['Volume', 'Dividends', 'Stock Splits'])
-        self.history = history
-        self.openList = history['Open']
-        self.closeList = history['Close']
-        self.highList = history['High']
-        self.lowList = history['Low']
-
-    def defineIndicators(self):
-        highList = self.highList
-        lowList = self.lowList
-        closeList = self.closeList
         
-        self.posDI = PLUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
-        self.negDI = MINUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
-        self.rsi = RSI(closeList, timeperiod=14)
-        self.stochk, self.stochd = STOCH(high=highList, low=lowList, close=closeList, fastk_period=5, slowk_period=3, slowd_period=3)
+        if interval == '1d':
+            self.dailyHistory = history
+            self.dailyOpenList = history['Open']
+            self.dailyCloseList = history['Close']
+            self.dailyHighList = history['High']
+            self.dailyLowList = history['Low']
+        else:
+            self.history = history
+            self.openList = history['Open']
+            self.closeList = history['Close']
+            self.highList = history['High']
+            self.lowList = history['Low']
+
+    def defineIndicators(self, interval):
+
+        if interval == '1d':
+            highList = self.dailyHighList
+            lowList = self.dailyLowList
+            closeList = self.dailyCloseList
+            self.dailyPosDI = PLUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
+            self.dailyNegDI = MINUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
+            self.dailyRSI = RSI(closeList, timeperiod=14)
+            self.dailyStochk, self.dailyStochd = STOCH(high=highList, low=lowList, close=closeList, fastk_period=5, slowk_period=3, slowd_period=3) 
+        else:      
+            highList = self.highList
+            lowList = self.lowList
+            closeList = self.closeList  
+            self.posDI = PLUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
+            self.negDI = MINUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
+            self.rsi = RSI(closeList, timeperiod=14)
+            self.stochk, self.stochd = STOCH(high=highList, low=lowList, close=closeList, fastk_period=5, slowk_period=3, slowd_period=3)
