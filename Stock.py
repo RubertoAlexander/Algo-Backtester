08841890f03cfd62fc2927df0d4bb22c35f39e10
Alexander Ruberto <rubertoalexander@gmail.com>
2020-09-15 20:53:14 +1000

    Refactoring

--- Stock.py
+++ Stock.py
@@ -12,34 +12,18 @@ class Stock:
 
     def setData(self, data, interval):
         
-        # if interval == '1d':
-        #     self.dailyHistory = data
-        #     self.dailyOpenList = data['Open']
-        #     self.dailyCloseList = data['Close']
-        #     self.dailyHighList = data['High']
-        #     self.dailyLowList = data['Low']
-        # else:
-            self.history = data
-            self.openList = data['Open']
-            self.closeList = data['Close']
-            self.highList = data['High']
-            self.lowList = data['Low']
+        self.history = data
+        self.openList = data['Open']
+        self.closeList = data['Close']
+        self.highList = data['High']
+        self.lowList = data['Low']
 
     def defineIndicators(self, interval):
 
-        # if interval == '1d':
-        #     highList = self.dailyHighList
-        #     lowList = self.dailyLowList
-        #     closeList = self.dailyCloseList
-        #     self.dailyPosDI = PLUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
-        #     self.dailyNegDI = MINUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
-        #     self.dailyRSI = RSI(closeList, timeperiod=14)
-        #     self.dailyStochk, self.dailyStochd = STOCH(high=highList, low=lowList, close=closeList, fastk_period=5, slowk_period=3, slowd_period=3) 
-        # else:      
-            highList = self.highList
-            lowList = self.lowList
-            closeList = self.closeList  
-            self.posDI = PLUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
-            self.negDI = MINUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
-            self.rsi = RSI(closeList, timeperiod=14)
-            self.stochk, self.stochd = STOCH(high=highList, low=lowList, close=closeList, fastk_period=5, slowk_period=3, slowd_period=3)
+        highList = self.highList
+        lowList = self.lowList
+        closeList = self.closeList  
+        self.posDI = PLUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
+        self.negDI = MINUS_DI(high=highList, low=lowList, close=closeList, timeperiod=14)
+        self.rsi = RSI(closeList, timeperiod=14)
+        self.stochk, self.stochd = STOCH(high=highList, low=lowList, close=closeList, fastk_period=5, slowk_period=3, slowd_period=3)
--- System.py
+++ System.py
@@ -6,97 +6,25 @@ import csv
 from StrategyBuilder import StrategyBuilder
 from Stock import Stock
 from Holding import Holding
+from watchlist import watchlist
 
 class System:
     
     #Class Variables
     RUN_DAILY = True
+    PERIOD = '3mo' #1d, 1mo, 3mo, 6mo, 1y
+    INTERVAL = '1d'
+
     runningDate = ''
     FEES = 10
     capital = 10000
     profit = 0
-    risk = 0.25
+    risk = 0.1
     minPos = capital * risk
     STOP_LOSS = 0.5
-
-    # watchlist = ['OCL.AX', 'TNE.AX', 'CGL.AX', 'BVS.AX', 'XRF.AX', 'PME.AX', 'EML.AX', 'PPS.AX', 'EOS.AX', 'EOF.AX', 'CLA.AX', 'HUB.AX', 'WTC.AX', 'NAN.AX', 'BOT.AX', 'APX.AX', 'MVP.AX', 'ALU.AX', 'PCK.AX', 'CUV.AX', 'IRE.AX', 'ZNO.AX', 'PNV.AX', 'TLX.AX', 'RAP.AX', 'SPL.AX', 'TTB.AX', 'GSS.AX', 'FLC.AX', 'NEA.AX', 'SKF.AX', 'MSB.AX', 'APT.AX', 'IPD.AX', 'PBH.AX', 'PPH.AX', 'KZA.AX', 'WSP.AX', 'AVH.AX', 'PAR.AX', 'OCC.AX', 'Z1P.AX', 'TYR.AX', 'THC.AX', 'CYP.AX', 'LVT.AX', 'ANO.AX', 'PTX.AX', 'HWH.AX', 'DW8.AX', 'IMU.AX', 'BIT.AX', 'SWF.AX', 'BRN.AX', 'RAC.AX', 'AR9.AX', 'NUH.AX', 'RCE.AX', 'ANP.AX', 'PAA.AX', 'IMC.AX', 'OSX.AX', 'PIQ.AX', 'IBX.AX', 'DXB.AX', 'VR1.AX']
-    watchlist = ['OCL.AX', 'TNE.AX', 'CGL.AX', 'BVS.AX', 'XRF.AX', 'PME.AX', 'EML.AX', 'PPS.AX', 'EOS.AX', 'CLA.AX', 'HUB.AX', 'WTC.AX', 'NAN.AX', 'BOT.AX', 'APX.AX', 'MVP.AX', 'ALU.AX', 'PCK.AX', 'CUV.AX', 'IRE.AX', 'ZNO.AX', 'PNV.AX', 'TLX.AX', 'RAP.AX', 'SPL.AX', 'TTB.AX', 'GSS.AX', 'FLC.AX', 'NEA.AX', 'SKF.AX', 'MSB.AX', 'APT.AX', 'IPD.AX', 'PPH.AX', 'KZA.AX', 'PAR.AX', 'OCC.AX', 'Z1P.AX', 'THC.AX', 'CYP.AX', 'LVT.AX', 'ANO.AX', 'PTX.AX', 'HWH.AX', 'DW8.AX', 'IMU.AX', 'BIT.AX', 'SWF.AX', 'BRN.AX', 'RAC.AX', 'AR9.AX', 'NUH.AX', 'RCE.AX', 'ANP.AX', 'PAA.AX', 'IMC.AX', 'PIQ.AX', 'IBX.AX', 'DXB.AX', 'VR1.AX']
-
-    #watchlist =['A2M.AX', 'AAC.AX', 'ABC.AX', 'ABP.AX', 'AFI.AX', 'AGL.AX', 'AIA.AX', 'ALD.AX', 'ALL.AX', 'ALQ.AX', 'ALU.AX', 'ALX.AX', 'AMC.AX', 'AMP.AX', 'ANN.AX', 'ANZ.AX', 'APA.AX', 'API.AX', 'APT.AX', 'APX.AX', 'ARB.AX', 'ARF.AX', 'ARG.AX', 'ASB.AX', 'AST.AX', 'ASX.AX', 'AVH.AX', 'AWC.AX', 'AX1.AX', 'AZJ.AX', 'BAP.AX', 'BBN.AX', 'BEN.AX', 'BGA.AX', 'BHP.AX', 'BIN.AX', 'BKL.AX', 'BKW.AX', 'BLD.AX', 'BLX.AX', 'BOQ.AX', 'BPT.AX', 'BRG.AX', 'BSL.AX', 'BVS.AX', 'BWP.AX', 'BXB.AX', 'CAR.AX', 'CBA.AX', 'CCL.AX', 'CCP.AX', 'CCX.AX', 'CGC.AX', 'CGF.AX', 'CHC.AX', 'CIM.AX', 'CKF.AX', 'CNU.AX', 'COH.AX', 'COL.AX', 'CPU.AX', 'CQR.AX', 'CSL.AX', 'CSR.AX', 'CTD.AX', 'CUV.AX', 'CWN.AX', 'CWY.AX', 'DHG.AX', 'DMP.AX', 'DOW.AX', 'DXS.AX', 'ECX.AX', 'EHE.AX', 'EHL.AX', 'ELD.AX', 'EML.AX', 'EVN.AX', 'FBU.AX', 'FLT.AX', 'FMG.AX', 'FNP.AX', 'FPH.AX', 'FXL.AX', 'GMA.AX', 'GMG.AX', 'GOR.AX', 'GOZ.AX', 'GPT.AX', 'GUD.AX', 'GWA.AX', 'HLS.AX', 'HSN.AX', 'HVN.AX', 'IAG.AX', 'IEL.AX', 'IEM.AX', 'IEU.AX', 'IFL.AX', 'IGO.AX', 'ILU.AX', 'IMD.AX', 'INA.AX', 'ING.AX', 'IPH.AX', 'IPL.AX', 'IRE.AX', 'IRI.AX', 'IVC.AX', 'IVV.AX', 'JBH.AX', 'JHG.AX', 'JHX.AX', 'KGN.AX', 'KLA.AX', 'LLC.AX', 'LNK.AX', 'LOV.AX', 'LYC.AX', 'MFG.AX', 'MGR.AX', 'MIN.AX', 'MMS.AX', 'MND.AX', 'MNF.AX', 'MP1.AX', 'MPL.AX', 'MQG.AX', 'MSB.AX', 'MTS.AX', 'NAB.AX', 'NAN.AX', 'NCM.AX', 'NEA.AX', 'NEC.AX', 'NHC.AX', 'NHF.AX', 'NSR.AX', 'NST.AX', 'NUF.AX', 'NWH.AX', 'NWL.AX', 'NWS.AX', 'NXT.AX', 'OFX.AX', 'OGC.AX', 'OML.AX', 'ORA.AX', 'ORE.AX', 'ORG.AX', 'ORI.AX', 'OSH.AX', 'OZL.AX', 'PGH.AX', 'PME.AX', 'PMV.AX', 'PPT.AX', 'PRN.AX', 'PRU.AX', 'PTM.AX', 'QAN.AX', 'QBE.AX', 'QUB.AX', 'REA.AX', 'REG.AX', 'RFF.AX', 'RHC.AX', 'RIO.AX', 'RMD.AX', 'RRL.AX', 'RSG.AX', 'RWC.AX', 'S32.AX', 'SAR.AX', 'SBM.AX', 'SCG.AX', 'SCP.AX', 'SDF.AX', 'SEK.AX', 'SFR.AX', 'SGF.AX', 'SGM.AX', 'SGP.AX', 'SGR.AX', 'SHL.AX', 'SHV.AX', 'SIQ.AX', 'SKC.AX', 'SKI.AX', 'SLF.AX', 'SOL.AX', 'SPK.AX', 'SSM.AX', 'STO.AX', 'STW.AX', 'SUL.AX', 'SUN.AX', 'SVW.AX', 'SYD.AX', 'TAH.AX', 'TCL.AX', 'TGR.AX', 'TLS.AX', 'TNE.AX', 'TPG.AX', 'TRS.AX', 'TWE.AX', 'URW.AX', 'UWL.AX', 'VCX.AX', 'VEA.AX', 'VOC.AX', 'VRL.AX', 'VRT.AX', 'WBC.AX', 'WEB.AX', 'WES.AX', 'WGX.AX', 'WHC.AX', 'WOR.AX', 'WOW.AX', 'WPL.AX', 'WPR.AX', 'WSA.AX', 'WTC.AX', 'XRO.AX']
-    # watchlist = {
-    #     'OCL.AX': 4.72,
-    #     'TNE.AX': 4.41, 
-    #     'CGL.AX': 3.54, 
-    #     'BVS.AX', 
-    #     'XRF.AX', 
-    #     'PME.AX', 
-    #     'CL1.AX', 
-    #     'PPS.AX', 
-    #     'EOS.AX', 
-    #     'EOF.AX', 
-    #     'EML.AX', 
-    #     'HUB.AX', 
-    #     'APX.AX', 
-    #     'WTC.AX', 
-    #     'BOT.AX', 
-    #     'MVP.AX', 
-    #     'ALU.AX', 
-    #     'NAN.AX', 
-    #     'PCK.AX', 
-    #     'CUV.AX', 
-    #     'IRE.AX', 
-    #     'ZNO.AX', 
-    #     'PNV.AX', 
-    #     'TLX.AX', 
-    #     'RAP.AX', 
-    #     'SPL.AX', 
-    #     'TTB.AX', 
-    #     'GSS.AX', 
-    #     'NEA.AX', 
-    #     'SKF.AX', 
-    #     'MSB.AX', 
-    #     'APT.AX', 
-    #     'FLC.AX', 
-    #     'IPD.AX', 
-    #     'PBH.AX', 
-    #     'PPH.AX', 
-    #     'KZA.AX', 
-    #     'WSP.AX', 
-    #     'AVH.AX', 
-    #     'PAR.AX', 
-    #     'OCC.AX', 
-    #     'IMU.AX', 
-    #     'Z1P.AX',
-    #     'TYR.AX', 
-    #     'THC.AX', 
-    #     'CYP.AX', 
-    #     'LVT.AX', 
-    #     'ANO.AX', 
-    #     'PTX.AX', 
-    #     'DW8.AX', 
-    #     'BIT.AX', 
-    #     'SWF.AX', 
-    #     'BRN.AX', 
-    #     'RAC.AX', 
-    #     'AR9.AX', 
-    #     'NUH.AX', 
-    #     'RCE.AX', 
-    #     'HWH.AX', 
-    #     'ANP.AX', 
-    #     'PAA.AX', 
-    #     'IMC.AX', 
-    #     'OSX.AX', 
-    #     'PIQ.AX', 
-    #     'IBX.AX', 
-    #     'DXB.AX', 
-    #     'VR1.AX'
-
-
-
-    # }
+    
     toPurchase = []
     holdings = []
-
     valueDict = dict()
 
     biggestGain = 0
@@ -141,12 +69,9 @@ class System:
                         # Sell if hit stop loss
                         for holding in self.holdings:
                             if holding.stock.code == stock.code:
-                                if holding.trailingStop == 0:
-                                    holding.setTrailingStop(holding.boughtAt * (1-self.STOP_LOSS))
-                                elif holding.trailingStop < holding.stock.currentPrice * (1-self.STOP_LOSS):
-                                    holding.setTrailingStop(holding.stock.currentPrice * (1-self.STOP_LOSS))
                                     
-                                if holding.stock.currentPrice <= holding.trailingStop:
+                                # if holding.stock.currentPrice <= holding.trailingStop:
+                                if holding.stock.currentPrice <= holding.boughtAt * (1-self.STOP_LOSS):
                                     print('Hit stop loss for', stock.code)
                                     self.sellAsStop(holding)
 
@@ -160,48 +85,10 @@ class System:
                         self.buy(stock, stock.closeList[i], 0.5)
                     elif (stratResult == 'Sell') & (self.haveHolding(stock)): #& (self.toPurchase.__contains__(stock)):
                         # self.toPurchase.remove(stock)
-
+                        print(self.runningDate, 'Sell', stock.code)
                         self.sell(stock, stock.closeList[i], 1)
                     elif  (stratResult == 'Sell2') & (self.haveHolding(stock)):
                         self.sell(stock, stock.closeList[i], 0.5)
-
-                    # elif (stratResult == 'Sell') & (self.RUN_DAILY):
-                    #     #Sell stock if greater than what was bought at
-                    #     toSell = []
-                    #     for holding in self.holdings:
-                    #         if (holding.stock.code == stock.code) & (stock.dailyCloseList[i] > holding.boughtAt):
-                    #             toSell.append(holding)
-                    #     if len(toSell) > 0: self.sell(toSell, stock.dailyCloseList[i])
-
-                    # #If cleared to purchase go through intraday indicators
-                    # if not self.RUN_DAILY:
-                    #     if self.toPurchase.__contains__(stock):
-                    #         intraday = stock.closeList.axes[0].date[j] #Intraday date
-
-                    #         while (intraday < self.runningDate) & (j < stock.closeList.axes[0].date.size):
-                    #             #Move date forward if needing to
-                    #             intraday = stock.closeList.axes[0].date[j]
-                    #             j+=1
-
-                    #         #Get Buy/Sell indicator for each interval intraday
-                    #         while (intraday == self.runningDate) & (j < stock.closeList.axes[0].date.size):
-                    #             stock.setCurrentPrice(stock.closeList[j])
-                    #             intraday = stock.closeList.axes[0].date[j]
-
-                    #             stratResult = stratBuilder.Strategy1(stock, j, '30m')
-
-                    #             if stratResult == 'Buy':
-                    #                 self.buy(stock, stock.closeList[j])
-                    #             elif (stratResult == 'Sell') & (self.haveHolding(stock)):
-                    #                 toSell = []
-                    #                 for holding in self.holdings:
-                    #                     if holding.stock.code == stock.code:
-                    #                         if stock.closeList[j] > holding.boughtAt:
-                    #                             toSell.append(holding)
-                    #                 if len(toSell) > 0: self.sell(toSell, stock.closeList[j])
-
-                    #             j+=1
-                    #         j = 0
             i+=1
 
         print('Buys: ', self.buyCount, '\n', 'Sells: ', self.sellCount)
@@ -226,33 +113,19 @@ class System:
         stocks = []
         dailyData = yf.download(
             tickers=self.watchlist,
-            # start='2018-01-01',
-            # end='2018-12-30',
-            period='1y',
-            interval='1d',
+            # start='2019-01-01',
+            # end='2019-12-30',
+            period=self.PERIOD,
+            interval=self.INTERVAL,
             group_by='ticker'
         )
-        if not self.RUN_DAILY:
-            intraData = yf.download(
-                tickers=self.watchlist,
-                start='2020-06-13',
-                end='2020-08-11',
-                # period='1mo',
-                interval='5m',
-                group_by='ticker'
-            )
                 
         for stock in self.watchlist:
             ticker = Stock(stock)
             stocks.append(ticker)
             #Get daily data
-            ticker.setData(dailyData[stock], '1d') if len(self.watchlist) > 1 else ticker.setData(dailyData, '1d')
-            ticker.defineIndicators('1d')
-
-            if not self.RUN_DAILY:
-                #Get intraday data
-                ticker.setData(intraData[stock], '30m') if len(self.watchlist) > 1 else ticker.setData(intraData, '1d')
-                ticker.defineIndicators('30m')
+            ticker.setData(dailyData[stock], self.INTERVAL) if len(self.watchlist) > 1 else ticker.setData(dailyData, self.INTERVAL)
+            ticker.defineIndicators(self.INTERVAL)
         
         return stocks
 
